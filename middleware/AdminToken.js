334e91e0d823d7485a66a4adcd4df4d6abeeec8d
santa8795 <santa879501@gmail.com>
2025-07-23 15:50:51 +0530

    auth improvment

--- middleware/AdminToken.js
+++ middleware/AdminToken.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const verifyToken = async (req, res, next) => {
     try {
         const token = req.header("Authorization")?.replace("Bearer ", "").trim();
+        console.log(token);
+        
         if (!token) {
             return res.status(401).json({ error: "Token is required" });
         }
--- routes/PlayerRoutes/TounamentAccessRoute.js
+++ routes/PlayerRoutes/TounamentAccessRoute.js
@@ -6,10 +6,11 @@ const {
 } = require('../../controllers/Player/TournamentAccessController');
 
 const playerVerification = require("../../middleware/PlayerToken");
+const AdminToken = require("../../middleware/AdminToken");
 const router = express.Router();
 
 router.get('/getmatch', playerVerification, getActiveTournaments);
 router.post('/joinmatch', playerVerification, joinTournament); // New route to join
-router.post('/sendMailToJoinedPlayers', playerVerification, sendMailToJoinedPlayers); // New route to join
+router.post('/sendMailToJoinedPlayers', AdminToken, sendMailToJoinedPlayers); // New route to join
 
 module.exports = router;
