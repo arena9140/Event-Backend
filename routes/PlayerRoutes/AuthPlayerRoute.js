cb36a93d7f1c5d6f99dd30cc7440cac80c38151c
santa8795 <santa879501@gmail.com>
2025-07-28 15:05:31 +0530

    player verification

--- routes/PlayerRoutes/AuthPlayerRoute.js
+++ routes/PlayerRoutes/AuthPlayerRoute.js
@@ -2,9 +2,11 @@ const express = require('express');
 const { Register,Login, getProfile} = require('../../controllers/Player/PlayerAuthController');
 const router = express.Router();
 
+const playerVerification = require("../../middleware/PlayerToken");
+
 
 router.post('/register',Register); 
 router.post('/login',Login); 
-router.post('/getProfile',getProfile); 
+router.post('/getProfile',playerVerification,getProfile); 
 
 module.exports = router;
